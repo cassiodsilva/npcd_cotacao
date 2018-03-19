4ff1ad82badb104f06fc0c0ab6bedbcffce7d844
cassiodsilva <36083138+cassiodsilva@users.noreply.github.com>
2018-03-18 21:36:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 
-var express = require('express');
-var app = express();
+'use strict';
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
 
